84f32f7bb93b1102a5d898f2f13ec7ed82c3d8e6
fhoyinn <whoisfhoyinn@gmail.com>
2022-04-20 20:09:38 +0100

    printf

--- main.h
+++ main.h
@@ -8,30 +8,68 @@
 #include <unistd.h>
 #include <limits.h>
 
-/**
-* struct pin - a structure that contains a function and a flag.
-* @flag: a character that identifies a function.
-* @fn: the pointer to a function.
-*/
+/**                                                                           
+ * struct flags - struct containing flags to "turn on"                        
+ * when a flag specifier is passed to _printf()                               
+ * @plus: flag for the '+' character                                          
+ * @space: flag for the ' ' character                                         
+ * @hash: flag for the '#' character                                          
+ */                                                                           
+typedef struct flags                                                          
+{                                                                             
+        int plus;                                                             
+        int space;                                                            
+        int hash;                                                             
+} flags_t;                                                                    
+                                                                              
+/**                                                                           
+ * struct printHandler - struct to choose the right function depending        
+ * on the format specifier passed to _printf()                                
+ * @c: format specifier                                                       
+ * @f: pointer to the correct printing function                               
+ */                                                                           
+typedef struct printHandler                                                   
+{                                                                             
+        char c;                                                               
+        int (*f)(va_list ap, flags_t *f);                                     
+} ph;                                                                         
+                                                                              
+/* print_nums */                                                              
+int print_int(va_list l, flags_t *f);                                         
+void print_number(int n);                                                     
+int print_unsigned(va_list l, flags_t *f);                                    
+int count_digit(int i);
 
-typedef struct pin
-{
-        char flag;
-        char *(*fn)(va_list args);
-} sir;
+/* print_bases */
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
 
+/* converter */
+char *convert(unsigned long int num, int base, int lowercase);
+
+/* _printf */
 int _printf(const char *format, ...);
-char *funct(const char symbol, va_list);
-void *print_str_cat(char *dest, char *src);
-char *print_str_char(va_list args);
-int print_str_concat(char *buff, const char *format, int n);
-char *print_str_integer(va_list args);
-int print_str_len(char *s);
-char *print_str_percent(va_list args);
-char *print_str_string(va_list args);
 
+/* get_print */
+int (*get_print(char s))(va_list, flags_t *);
 
+/* get_flag */
+int get_flag(char s, flags_t *f);
 
+/* print_alpha */
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
 
-#endif
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
 
+/* print_custom */
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
+
+/* print_address */
+int print_address(va_list l, flags_t *f);
